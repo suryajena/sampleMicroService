c7cdf09100773e203f34acb7564fd8465f0e5321
surya <surya.jena@yahoo.com>
2017-04-12 21:06:50 -0400

    sampleMicroservice: 	endpoint corrected

--- src/main/java/org/surya/ms/sample/controller/TestController.java
+++ src/main/java/org/surya/ms/sample/controller/TestController.java
@@ -1,10 +1,8 @@
 package org.surya.ms.sample.controller;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -15,29 +13,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 	 
-	@RequestMapping(value = "/tenants", method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.CREATED)
-	public String createSome(final @PathVariable String name) {
-		return "Hello"+name;
+	@RequestMapping(value = "/users/{name}", method = RequestMethod.GET)
+	public String getHello(final @PathVariable String name) {
+		return "Hello "+name+", Welcome to Java world.";
 	}
-	 
 	
-	/**
-	 * Returns a page of audit events for the specified {@code tenantId} using
-	 * the {@code filter} and {@code pageable} information.
-	 * 
-	 * @param id
-	 * @param filter
-	 * @param pageable
-	 * @return page of matching events
-	 */
-	/*@RequestMapping(value = "/users/{id}/audit-events", method = RequestMethod.GET)
-	public Page<AuditEventResource> getUserAuditEvents(
-			final @PathVariable String id,
-			final @QuerydslPredicate(root = AuditEvent.class) Predicate filter,
-			final @PageableDefault(size = 50, sort = "created", direction = Sort.Direction.DESC) Pageable pageable) {
-		return _auditEventService.getAuditEvents(id, filter, pageable)
-				.map(e -> _mapper.map(e, AuditEventResource.class));
+	/*@RequestMapping(value = "/users", method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public TenantResource createTenant(final @RequestBody @Validated(Insertable.class) User user) {
+		return _userService.create(user);
 	}*/
-	
+	 
 }
